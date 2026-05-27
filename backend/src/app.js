2cc1de4506c3f960d96221bdfc754defadaec7a7
push123drivedi <123721600+push123drivedi@users.noreply.github.com>
2026-05-27 12:06:34 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,13 +5,15 @@ import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    https://hhhh-fcqaypra6-push123drivedis-projects.vercel.app
-  ],
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://hhhh-fcqaypra6-push123drivedis-projects.vercel.app"
+    ],
+    credentials: true
+  })
+);
 
 app.use(express.json());
 
